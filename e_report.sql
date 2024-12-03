84ce8562e2ad48dc6a3fabdd3f611b02bed5ee37
Zadarudo <rajamalaikha@gmail.com>
2024-12-03 15:01:22 +0700

    Initial Commit

---
+++ e_report.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2024 at 01:53 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `e_report`
+--
+CREATE DATABASE IF NOT EXISTS `e_report` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `e_report`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `id` int(11) NOT NULL,
+  `issuer_id` int(128) NOT NULL,
+  `subject` text NOT NULL,
+  `message` text NOT NULL,
+  `date_of_reporting` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `email` text NOT NULL,
+  `role` varchar(32) NOT NULL,
+  `pw_hash` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `role`, `pw_hash`) VALUES
+(2, 'admin', 'admin@gmail.com', 'admin', 'cd5ea73cd58f827fa78eef7197b8ee606c99b2e6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_reporter` (`issuer_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `FK_reporter` FOREIGN KEY (`issuer_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+require_once "./helper_php/login_post.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Report | Login Page</title>
+    <style>
+      * {
+        
+      }
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+<body style="background:white;">
+    <div class="container d-flex flex-column align-items-center justify-content-center w-50" style="height:100vh">
+      <br>
+      <br>
+    <h1 class="font-weight-bold mb-5 ">Login into E-Report</h1>
+    <form action="" method="POST" class="w-100">
+
+
+  <div class="form-group">
+    <label  class="font-weight-bold">Email address</label>
+    <input type="email" class="form-control" name="email" placeholder="Enter email">
+    <p style="color:#ffe847;font-weight:600;text-shadow:none">
+    <?php if (!empty($emailErrorMassage)) {
+    echo $emailErrorMassage;}?>
+  </p>
+    </div>
+  <div class="form-group">
+    <label class=" font-weight-bold mt-1">Password</label>
+    <input type="password" class="form-control" name="password" placeholder="Password">
+    <p style="color:#ffe847;font-weight:600;text-shadow:none">
+    <?php if (!empty($passwordErrorMassage)) {
+    echo $passwordErrorMassage;}?>
+  </p>
+  </div>
+
+  <button type="submit" class="btn btn-primary my-2">Login</button>
+
+  <p>Don't have account?<a style="color:blue" class="pl-1" href="/registration.php">Registration</a></p>
+</form>
+        <div class="card " style="background: #00000014;">
+        <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <th> Email</th>
+                        <th> Password</th>
+                        <th> Role</th>
+                    </thead>
+                    <tbody>
+                    <tr>
+                            <td scope="col">admin@gmail.com</td>
+                            <td scope="col">admin123456</td>
+                            <td scope="col">admin</td>
+
+                        </tr>
+                    </tbody>
+                    </table>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
